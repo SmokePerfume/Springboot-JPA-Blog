275ccbe62221bb4b0d837b5dc20f4c046ceed802
smokeperfume <wjdgnstls97@naver.com>
2022-12-20 14:53:57 +0900

    환경세팅 완료

---
+++ src/main/java/com/hoon/blog/test/BlogControllerTest.java
@@ -0,0 +1,12 @@
+package com.hoon.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+		@GetMapping("/test/hello")
+		public String Hello() {
+			return "<h1>Hello World!</h1>";
+		}
+}
